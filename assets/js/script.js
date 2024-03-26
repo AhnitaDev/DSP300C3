2281754488035ecbec04236ddccf5c1368d1659b
AhnitaDev <osujianitachinwendu@gmail.com>
2024-03-26 11:59:23 +0100

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// alert("Hello world");
+// console.log("Hello world");
+// console.log("hi");
+
+// var x = 25;
+// var name1 = 'John'
+// var loading = true
+// var number = x
+
+// console.log(x)
+// console.log(name1)
+// console.log(loading)
+// console.log(number)
+
+// var num1 = 10;
+// var num2 =  1;
+// var sum = num1 + num2
+
+// console.log(sum)
+if (5 == 3){
+    console.log("Yes")
+}
+else {
+    console.log("false")
+}
+
